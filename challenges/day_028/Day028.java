c9301391118b228db64a980a57bef8db0798a840
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-12-29 09:16:06 -0300

    Day #028 complete

---
+++ challenges/day_028/Day028.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day028 {
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\edval\\IdeaProjects\\200-days-of-Java\\challenges\\day_028\\arquivo.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+

05762998c52949ce7d14ab2623b6876b977284f2
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-12-30 08:24:36 -0300

    Day #029 complete

---
+++ challenges/day_029/Day029.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Day029 {
+
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de linhas da matriz: ");
+        int linhas = scanner.nextInt();
+        System.out.print("Digite o número de colunas da matriz: ");
+        int colunas = scanner.nextInt();
+
+        int[][] matriz = new int[linhas][colunas];
+        System.out.println("Digite os elementos da matriz: ");
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        int[][] transposta = new int[colunas][linhas];
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                transposta[j][i] = matriz[i][j];
+            }
+        }
+
+        System.out.println("\nMatriz Transposta:");
+        for (int i = 0; i < colunas; i++) {
+            for (int j = 0; j < linhas; j++) {
+                System.out.print(transposta[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+
+}
+

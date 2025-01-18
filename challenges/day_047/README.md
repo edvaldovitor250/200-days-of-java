97a6c43657cdbae72a5fc579bbc301cf36c449e4
Edvaldo <edvaldovitor250@gmail.com>
2025-01-18 08:45:45 -0300

    Day #048 complete

--- challenges/day_047/README.md
+++ challenges/day_047/README.md
@@ -8,3 +8,23 @@
 
 ```java
 
+
+public class Day047 {
+    public static void main(String[] args) {
+        String texto = "O rato roeu a roupa do rei de Roma.";
+        String palavra = "roeu";
+    }
+
+   public static int contarPalavra(String texto, String palavra) {
+        String[] palavras = texto.split(" ");
+        int contador = 0;
+        for (String p : palavras) {
+            if (p.equals(palavra)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
+
+
---
+++ challenges/day_048/Day048.java
@@ -0,0 +1,22 @@
+public class Day048 {
+
+    public static void main(String[] args) {
+        String texto = "O rato roeu a roupa do rei de Roma.";
+        String letra = "a";
+
+        int resultado = contarLetra(texto, letra);
+        System.out.println("A letra '" + letra + "' aparece " + resultado + " vezes no texto.");
+    }
+
+    public static int contarLetra(String texto, String letra) {
+        int count = 0;
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (String.valueOf(texto.charAt(i)).equalsIgnoreCase(letra)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

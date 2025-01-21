b61cef9ee87088122f4143c2cf8560ef953f9bfe
Edvaldo <edvaldovitor250@gmail.com>
2025-01-20 21:12:23 -0300

    Day #050 complete

---
+++ challenges/day_050/Day050.java
@@ -0,0 +1,35 @@
+public class Day050 {
+
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de linhas da matriz: ");
+        int linhas = scanner.nextInt();
+        System.out.print("Digite o número de colunas da matriz: ");
+        int colunas = scanner.nextInt();
+
+        int[][] matriz = new int[linhas][colunas];
+
+        System.out.println("Digite os elementos da matriz:");
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("\nSoma das linhas:");
+        for (int i = 0; i < linhas; i++) {
+            int soma = 0;
+            for (int j = 0; j < colunas; j++) {
+                soma += matriz[i][j];
+            }
+            System.out.println("Soma da linha " + i + ": " + soma);
+        }
+
+        scanner.close();
+    }
+
+
+}

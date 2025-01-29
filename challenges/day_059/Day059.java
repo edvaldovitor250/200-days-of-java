d0dd9a50142ae8f4bf2388fcf4831757c7eb01c0
Edvaldo <edvaldovitor250@gmail.com>
2025-01-29 19:55:40 -0300

    Day #059 complete

---
+++ challenges/day_059/Day059.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Day059 {
+
+    public static void main(String[] args) {
+        Day059 day059 = new Day059();
+        Random random = new Random();
+        double[] notas = new double[10];
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = random.nextDouble() * 10;
+        }
+        System.out.println(day059.calculoMedia(notas));
+    }
+    
+
+    public double calculoMedia(double... notas){
+
+        double media = 0;
+        for (double nota : notas) {
+            media += nota;
+        }
+        return media / notas.length;
+
+    }
+}

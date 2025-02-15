0b31a0553165ba5df96145d108bbc6a3ca1fc34f
Edvaldo <edvaldovitor250@gmail.com>
2025-02-14 22:08:46 -0300

    Day #074 complete

---
+++ challenges/day_074/Day074.java
@@ -0,0 +1,25 @@
+public class Day074 {
+
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        numerosParesEIMpares(numeros);
+    }
+
+    public static void numerosParesEIMpares(int[] numeros) {
+        int pares = 0;
+        int impares = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+
+        System.out.println("Pares: " + pares);
+        System.out.println("Impares: " + impares);
+    }
+
+}
---
+++ challenges/day_074/README.md
@@ -0,0 +1,9 @@
+# Day 74
+
+## Desafio:
+
+	Crie um programa Java que leia um array de números e imprima os pares e ímpares separadamente
+    
+**Resultado:**
+
+```java

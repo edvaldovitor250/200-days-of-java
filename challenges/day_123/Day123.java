290902cc58789e373fc6437251325ff6bbfe4a44
Edvaldo <edvaldovitor250@gmail.com>
2025-04-01 22:07:32 -0300

    Day #123 complete

---
+++ challenges/day_123/Day123.java
@@ -0,0 +1,25 @@
+public class Day123{
+
+public static void main(String[] args) {
+    int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    agrupeImparesEpares(numeros);
+}
+
+public static void agrupeImparesEpares(int[] numeros) {
+    List<Integer> pares = new ArrayList<>();
+    List<Integer> impares = new ArrayList<>();
+
+    for (int numero : numeros) {
+        if (numero % 2 == 0) {
+            pares.add(numero);
+        } else {
+            impares.add(numero);
+        }
+    }
+
+    System.out.println("Números Pares: " + pares);
+    System.out.println("Números Ímpares: " + impares);
+  
+}
+
+}

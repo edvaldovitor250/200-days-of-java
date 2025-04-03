a312bae856824a5db123ded7f52d2d52ba2f3002
Edvaldo <edvaldovitor250@gmail.com>
2025-04-03 19:06:44 -0300

    Day #125 complete

---
+++ challenges/day_125/Day125.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Day125 {
+
+    public static void main(String[] args) {
+        List<Integer> pares = List.of(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+        calcularParesValoresEspecifico(pares);
+    }
+
+    public static void calcularParesValoresEspecifico(List<Integer> pares){
+        for (int iterable_element : pares) {
+            for (int i = 0; i < pares.size(); i++) {
+                for (int j = i + 1; j < pares.size(); j++) {
+                    if (pares.get(i) + pares.get(j) == iterable_element) {
+                        System.out.println("Pares encontrados: " + pares.get(i) + " e " + pares.get(j));
+                    }
+                }
+            }
+        }
+    }
+
+}   

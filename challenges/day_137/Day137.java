004fff2cd545e4f8ec3285e6a4e1a7ece29f8ec4
Edvaldo <edvaldovitor250@gmail.com>
2025-04-15 08:21:39 -0300

    Day #137 complete

---
+++ challenges/day_137/Day137.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Day137 {
+
+    public static void main(String[] args) {
+        Day137 day137 = new Day137();
+        int[] a = {1, 2, 3, 4, 5};
+        System.out.println(day137.isMonotonico(a)); 
+        int[] b = {5, 4, 3, 2, 1};
+        System.out.println(day137.isMonotonico(b));
+        int[] c = {1, 2, 3, 2, 1};
+        System.out.println(day137.isMonotonico(c)); 
+    }
+
+    public boolean isMonotonico(int []a){
+        if (a.length <= 2) return true;
+        boolean increasing = false, decreasing = false;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > a[i - 1]) {
+                increasing = true;
+            } else if (a[i] < a[i - 1]) {
+                decreasing = true;
+            }
+            if (increasing && decreasing) return false;
+        }
+        return true;
+    }
+
+}

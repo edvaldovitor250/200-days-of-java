81007de820259288fe524f4f3d1ea747418e67af
Edvaldo <edvaldovitor250@gmail.com>
2025-04-18 10:53:45 -0300

    Day #140 complete

---
+++ challenges/day_140/Day140.java
@@ -0,0 +1,34 @@
+public class Day140 {
+
+    public static void main(String[] args) {
+        System.out.println("Fibonacci Series:");
+        fibonacci(10); 
+
+        System.out.println("\nPrime Numbers:");
+        primeNumbers(50); 
+
+        System.out.println("\nArmstrong Number Check:");
+        numeroArmstrong(153);
+    }
+
+
+
+    public static void numeroArmstrong(int n){
+        int num = n, sum = 0, digits = 0;
+        while (num != 0) {
+            num /= 10;
+            digits++;
+        }
+        num = n;
+        while (num != 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        if (sum == n) {
+            System.out.println(n + " is an Armstrong number.");
+        } else {
+            System.out.println(n + " is not an Armstrong number.");
+        }
+    }
+}

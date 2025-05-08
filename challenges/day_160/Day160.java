7701dbfe1a484f62217d0bc9f197b1ed38b8c248
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-05-08 18:38:29 -0300

    Day #160 complete

---
+++ challenges/day_160/Day160.java
@@ -0,0 +1,21 @@
+public class Day160 {
+
+    public static int contarInversoes(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 4, 1, 3, 5};
+
+        int totalInversoes = contarInversoes(array);
+        System.out.println("Número de inversões: " + totalInversoes);
+    }
+}

e0270f97de3789989b735d47d1c9b373d94dd571
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-05-24 08:21:49 -0300

    Day #175 complete

---
+++ challenges/day_175/Day175.java
@@ -0,0 +1,27 @@
+
+public class Day175 {
+
+    public static void main(String[] args) {
+        somarBordas(new int[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+    }
+
+    public static void somarBordas(int[][] matriz){
+        int soma = 0;
+        int linhas = matriz.length;
+        int colunas = matriz[0].length;
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (i == 0 || i == linhas - 1 || j == 0 || j == colunas - 1) {
+                    soma += matriz[i][j];
+                }.
+            }
+        }
+        System.out.println("A soma dos elementos das bordas é: " + soma);
+    }
+
+}

872de3670416fd1481f21b7f04542dc53a9c7e18
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-05-28 08:47:33 -0300

    Day #179 complete

---
+++ challenges/day_179/Day179.java
@@ -0,0 +1,24 @@
+
+public class Day179 {
+
+    public static void main(String[] args) {
+        verificadoNumeroRepetidos(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+    }
+
+    public static void verificadoNumeroRepetidos(int[]numeros){
+        int[] contador = new int[10];
+
+        for (int numero : numeros) {
+            if (numero >= 0 && numero < 10) {
+                contador[numero]++;
+            }
+        }
+        for (int i = 0; i < contador.length; i++) {
+            if (contador[i] > 1) {
+                System.out.println("Numero " + i + " se repite " + contador[i] + " vezes.");
+            }
+        }
+
+    }
+
+}

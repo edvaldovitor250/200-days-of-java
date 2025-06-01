78efb2c473e024bf7fea9d1bc775b638cb9ab12a
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-06-01 10:17:12 -0300

    Day #183 complete

---
+++ challenges/day_183/Day183.java
@@ -0,0 +1,36 @@
+
+public class Day183 {
+
+    public static void main(String[] args) {
+        String[] palavras = {"radar", "level", "hello", "world", "madam", "racecar", "noon"};
+        palindromo(palavras);
+    }
+    
+    public static void palindromo(String[] palavras) {
+        String maiorPalindromo = "";
+        
+        for (String palavra : palavras) {
+            if (isPalindromo(palavra) && palavra.length() > maiorPalindromo.length()) {
+                maiorPalindromo = palavra;
+            }
+        }
+        
+        System.out.println("Maior palíndromo encontrado: " + maiorPalindromo);
+    }
+    public static boolean isPalindromo(String palavra) {
+        int left = 0;
+        int right = palavra.length() - 1;
+        
+        while (left < right) {
+            if (palavra.charAt(left) != palavra.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+
+
+}
